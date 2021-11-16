2045d28e03da16d52752b1407e8500c38503bcbc
sammy <sammyoina@gmail.com>
2021-11-16 08:41:29 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,75 +1,23 @@
-/*package main
-
-import (
-	"encoding/json"
-	"fmt"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	"github.com/IBM/go-sdk-core/core"
-	"github.com/watson-developer-cloud/go-sdk/v2/assistantv2"
-)
-
-func main() {
-	authenticator := &core.IamAuthenticator{
-		ApiKey: "HXoxv39NR1RyhzCxsJYhNCm9tFKbL1kni0mmnpJW7Fn5",
-	}
-
-	options := &assistantv2.AssistantV2Options{
-		Version:       "2021-06-14",
-		Authenticator: authenticator,
-	}
-
-	assistant, assistantErr := assistantv2.NewAssistantV2(options)
-
-	if assistantErr != nil {
-		panic(assistantErr)
-	}
-
-	assistant.SetServiceURL("https://api.eu-gb.assistant.watson.cloud.ibm.com/instances/c630716a-83de-4e23-bb90-d51e23c6c5a0/v2/assistants/1e108d47-e09f-4b70-b0c4-a8b1e0ac1813/sessions")
-
-	result, _, responseErr := assistant.Message(
-		&assistantv2.MessageOptions{
-			AssistantID: core.StringPtr("1e108d47-e09f-4b70-b0c4-a8b1e0ac1813"),
-			SessionID:   core.StringPtr("{session_id}"),
-			Input: &assistantv2.MessageInput{
-				MessageType: core.StringPtr("text"),
-				Text:        core.StringPtr("Hello"),
-			},
-		},
-	)
-	if responseErr != nil {
-		panic(responseErr)
-	}
-	b, _ := json.MarshalIndent(result, "", "  ")
-	fmt.Println(string(b))
-}
-*/
 package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/IBM/go-sdk-core/core"
+	"github.com/joho/godotenv"
 	"github.com/watson-developer-cloud/go-sdk/v2/assistantv2"
 )
 
 func main() {
-	ver := "2021-11-16"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env File")
+	}
+	ver := os.Getenv("version")
 	authenticator := &core.IamAuthenticator{
-		ApiKey: "HXoxv39NR1RyhzCxsJYhNCm9tFKbL1kni0mmnpJW7Fn5",
+		ApiKey: os.Getenv("apiKey"),
 	}
 
 	options := &assistantv2.AssistantV2Options{
@@ -83,10 +31,10 @@ func main() {
 		panic(assistantErr)
 	}
 
-	assistant.SetServiceURL("https://api.eu-gb.assistant.watson.cloud.ibm.com")
+	assistant.SetServiceURL(os.Getenv("serviceUrl"))
 
 	result, _, responseErr := assistant.CreateSession(assistant.
-		NewCreateSessionOptions("1e108d47-e09f-4b70-b0c4-a8b1e0ac1813"))
+		NewCreateSessionOptions(os.Getenv("assistantId")))
 	if responseErr != nil {
 		panic(responseErr)
 	}
